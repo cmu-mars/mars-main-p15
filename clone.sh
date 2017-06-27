d0ec5c77528e24deda47f298a725492fd4c16982
Ian Voysey <iev@cs.cmu.edu>
2017-06-27 12:29:47 -0400

    added a total hack script to clone the repos into the directory containing this one, assuming the name is exactly what the repo name is

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ..
+git clone git@github.com:cmu-mars/brasscomms.git
+git clone git@github.com:cmu-mars/ros-topic-probe-p15
+git clone git@github.com:cmu-mars/notifications-p15
+git clone git@github.com:cmu-mars/ig-action-server-p15
+git clone git@github.com:cmu-mars/cp-gazebo-p15
+cd mars-main-p15/
